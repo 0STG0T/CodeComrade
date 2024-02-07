2a8eff79937260b27e8c54e59e7cdb8a66fdddf4
0STG0T <ostgot116@gmail.com>
2024-02-07 14:26:01 +0300

    the change

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# CodeComrade
-A tg bot that helps students with code understanding and possibly will have couple more features.
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="./images/logo.webp" alt="Bot logo"></a>
+</p>
+
+<h3 align="center">CodeComrade</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-inactive-red.svg)]()
+[![Platform](https://img.shields.io/badge/platform-telegram-blue.svg)](https://t.me/code_comrade_bot)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](/LICENSE)
+
+</div>
+
+---
+
+<p align="center">A real friend won't let you cheat!<br>🤖<br>
+CodeComrade - a TG bot that helps students by providing helpful hints for different problems, but not giving out the whole solution to copy.
+    <br> 
+</p>
+
+## 📝 Table of Contents
+
+- [About](#about)
+- [Demo / Working](#demo)
+- [How it works](#working)
+- [Usage](#usage)
+- [Deploying your own bot](#deployment)
+- [TODO](../TODO.md)
+- [Acknowledgements](#acknowledgement)
+
+## 🧐 About <a name = "about"></a>
+<b>Problem:</b><br>
+<pre>
+  When solving problems, many people have problems, sometimes a person does not understand where to start a solution. In such circumstances, cheating or mindless copying of the solution often occurs. Thus, a person does not learn and forgets the solution over time, instead of learning how to solve this problem.
+</pre>
+  <br>
+<b>Solution:</b><br>
+<pre>
+  A telegram bot that gives hints and leads to a solution without giving a complete solution.
+</pre>
+
+## 🎥 Demo / Working <a name = "demo"></a>
+
+![Responce with secrets](./images/demo1.png)
+
+![Unleashing secrts](./images/demo2.png)
+
+## 💭 How it works <a name = "working"></a>
+<b>1. Getting responce from OpenAI:</b>
+<pre>
+response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are an assistant skilled in explaining olympiad programming tasks who speaks russian language and provides useful links for resources when its is needed. You should give 3 levels of hints: Hint 1: (easy hint), Hint2: (medium hint), Hint 3: (full hint). If a certain algorithm is needed, state the name of it in the Hint 2. Also provide code in Hint 3. Format the output like this: Hint 1: ... Hint 2: ... Hint 3: ... ."},
+                    {"role": "user", "content": f"Помоги с решением этой задачи:\n\n{text}"}
+                ]
+      )
+</pre>
+<b>2. Formatting telegram output:</b>
+<pre>
+def secretize_hint(hint, n):
+    hint = hint.split('\n')
+    for i in range(len(hint)):
+        hint[i] = f'||{hint[i]}||'
+    hint = f"Подсказка {n}:\n" + '\n'.join(hint)
+    
+    return hint
+  
+for c in ['_', '`', '*', '[', ']', '(', ')', '~', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!' ]:
+                explanation = explanation.replace(c, f'\\{c}')
+
+hint1 = explanation[explanation.find("Hint 1:"):explanation.find("Hint 2:")]
+            hint1 = secretize_hint(hint1, 1)
+</pre>
+
+## 🎈 Usage <a name = "usage"></a>
+
+@code_comrade_bot
+
+Just send it a problem.
+
+The bot will then give you 3 hints in secrets.
+
+## 🚀 Deploying your own bot <a name = "deployment"></a>
+
+<pre>
+1. Install all packages used in the project.
+2. Pass your API keys to mysecrets.py
+3. RUN bot.py
+</pre>
+
+## ⛏️ Built Using <a name = "built_using"></a>
+
+- OpenAI API
+- BotFather
+- Telebot
+- Python
+
+## 🎉 Acknowledgements <a name = "acknowledgement"></a>
+
+Project paper and presentaion in russian: [link](./АкмаевА_11А%202.zip)
--- bot/bot.py
+++ bot/bot.py
@@ -13,22 +13,31 @@ OPENAI_API_KEY = config.openai_api_key
 client = OpenAI(api_key=OPENAI_API_KEY)
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+def secretize_hint(hint, n):
+    hint = hint.split('\n')
+    for i in range(len(hint)):
+        hint[i] = f'||{hint[i]}||'
+    hint = f"Подсказка {n}:\n" + '\n'.join(hint)
+    
+    return hint
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hi! Send me some code, and I'll try to explain it.")
+    bot.reply_to(message, "Привет! Поришли мне задачку, я дам тебе подсказки.")
     
 # main function
 @bot.message_handler(func=lambda message: True)
 def explain_code(message):
-    code = clean_code(message.text)
+    text = message.text
     
     # cache
-    conn = sqlite3.connect(config.cache_dir + 'code_explanations.db')
-    cursor = conn.cursor()
+    #conn = sqlite3.connect(config.cache_dir + 'code_explanations.db')
+    #cursor = conn.cursor()
     # Check if the code is already in the database
-    cursor.execute("SELECT explanation FROM explanations WHERE code = ?", (code,))
-    explanation = cursor.fetchone()
+    #cursor.execute("SELECT explanation FROM explanations WHERE code = ?", (code,))
+    #explanation = cursor.fetchone()
+    
+    explanation = None
     
     if explanation:
         bot.reply_to(message, explanation[0])
@@ -37,17 +46,41 @@ def explain_code(message):
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {"role": "system", "content": "You are an assistant skilled in explaining programming code who speaks russian language and provides useful links for resources when its is needed."},
-                    {"role": "user", "content": f"Обьясни этот код:\n\n{code}"}
+                    {"role": "system", "content": "You are an assistant skilled in explaining olympiad programming tasks who speaks russian language and provides useful links for resources when its is needed. You should give 3 levels of hints: Hint 1: (easy hint), Hint2: (medium hint), Hint 3: (full hint). If a certain algorithm is needed, state the name of it in the Hint 2. Also provide code in Hint 3. Format the output like this: Hint 1: ... Hint 2: ... Hint 3: ... ."},
+                    {"role": "user", "content": f"Помоги с решением этой задачи:\n\n{text}"}
                 ]
             )
             explanation = response.choices[0].message.content
             
-            # Insert the new code and explanation into the database
-            cursor.execute("INSERT INTO explanations (code, explanation) VALUES (?, ?)", (code, explanation))
-            conn.commit()
+            for c in ['_', '`', '*', '[', ']', '(', ')', '~', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!' ]:
+                explanation = explanation.replace(c, f'\\{c}')
             
-            bot.reply_to(message, explanation)
+            hint1 = explanation[explanation.find("Hint 1:"):explanation.find("Hint 2:")]
+            hint1 = secretize_hint(hint1, 1)
+            
+            hint2 = explanation[explanation.find("Hint 2:"):explanation.find("Hint 3:")]
+            hint2 = secretize_hint(hint2, 2)
+            
+            hint3 = explanation[explanation.find("Hint 3:"):]
+            hint3 = secretize_hint(hint3, 3)
+            
+            all_hints = '\n\n'.join([hint1, hint2, hint3])
+            
+            # Insert the new code and explanation into the database
+            #cursor.execute("INSERT INTO explanations (code, explanation) VALUES (?, ?)", (code, explanation))
+            #conn.commit()
+            try:
+                bot.reply_to(message, hint1, parse_mode='MarkdownV2')
+            except:
+                print(hint1)
+            try:
+                bot.reply_to(message, hint2, parse_mode='MarkdownV2')
+            except:
+                print(hint2)
+            try:
+                bot.reply_to(message, hint3, parse_mode='MarkdownV2')
+            except:
+                print(hint3)
             
         except Exception as e:
             bot.reply_to(message, f"An error occurred: {e}")
